09f0f3e52e3b7a473018acce947f6c321c91393f
sirdesmond09 <sirdesmond09@gmail.com>
2020-05-07 18:03:26 +0100

    enabling search

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import post_detail, post_list, post_share #PostList
+from .views import post_detail, post_list, post_share, post_search #PostList
 from .feeds import LatestPostsFeed
 
 
@@ -10,6 +10,7 @@ urlpatterns = [
     path('tag/<slug:tag_slug>/',post_list, name='post_list_by_tag'),
     # path('', PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='post_detail'),
+    path('search/', post_search, name='post_search'),
     path('<int:post_id>/share/', post_share, name = 'post_share'),
     path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,10 +3,12 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector #allows us to search within our blog
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from taggit.models import Tag
 from django.db.models import Count
+
 
 
 def post_list(request, tag_slug =None):
@@ -112,3 +114,24 @@ def post_share(request, post_id):
         'sent': sent,
     }
     return render(request, 'blog/post/share.html', context)
+
+def post_search(request):
+    form    = SearchForm
+    query   = None
+    results = []
+
+    if 'query' in request.GET:
+        form =SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', weight='A') + \
+                            SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(search= search_vector, rank = SearchRank(search_vector, search_query)).filter(rank__gte=0.3).order_by('-rank')
+
+    context = {
+            'results': results,
+            'form': form,
+            'query': query,
+        }
+    return render(request, 'blog/post/search.html', context)
