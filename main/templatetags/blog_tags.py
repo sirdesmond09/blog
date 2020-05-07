f2bfd5dcc12106dc3c5e8053c033e29bb06a249e
sirdesmond09 <sirdesmond09@gmail.com>
2020-05-07 08:57:53 +0100

    custom template tags and filter

--- main/templatetags/blog_tags.py
+++ main/templatetags/blog_tags.py
@@ -2,10 +2,15 @@
 that allow you to create your own template tags in an easy manner: 
 
 1. simple_tag: Processes the data and returns a string
-2. inclusion_tag: Processes the data and returns a rendered template '''
+2. inclusion_tag: Processes the data and returns a rendered template 
+3. filter: This is used for registering custom filters'''
 
 from django import template
 from main.models import Post
+from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
+
 
 register = template.Library()
 
@@ -13,4 +18,18 @@ register = template.Library()
 def total_posts():
     return Post.published.count()
 
-    
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    context = {
+        'latest_posts': latest_posts
+    }
+    return(context)
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
